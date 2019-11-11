d4010c48f6141307b264d361d43496bbb67daafa
Rostislav Svoboda <rsvoboda@redhat.com>
2019-11-11 11:52:32 +0100

    quarkus-hibernate-orm works now in native without entities

--- 101-javaee-like-getting-started/src/test/java/io/quarkus/qe/core/JavaEELikeHealthCheckTest.java
+++ 101-javaee-like-getting-started/src/test/java/io/quarkus/qe/core/JavaEELikeHealthCheckTest.java
@@ -16,8 +16,8 @@ public class JavaEELikeHealthCheckTest {
                 .then()
                 .statusCode(200)
                 .body("status", is("UP"),
-                        "checks.name", hasItems("liveness", "readiness", "greeting"),
-                        "checks.status", hasSize(3),
+                        "checks.name", containsInAnyOrder("liveness", "readiness", "greeting", "Database connection(s) health check"),
+                        "checks.status", hasSize(4),
                         "checks.status", hasItem("UP"),
                         "checks.status", not(hasItem("DOWN"))
                 );
@@ -29,8 +29,8 @@ public class JavaEELikeHealthCheckTest {
                 .then()
                 .statusCode(200)
                 .body("status", is("UP"),
-                        "checks.name", contains("readiness"),
-                        "checks.status", hasSize(1),
+                        "checks.name", containsInAnyOrder("readiness", "Database connection(s) health check"),
+                        "checks.status", hasSize(2),
                         "checks.status", hasItem("UP")
                 );
     }
