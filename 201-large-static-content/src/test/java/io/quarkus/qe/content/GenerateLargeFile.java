88dee5f5362290280a957ec5607104dd314231a2
Rostislav Svoboda <rsvoboda@redhat.com>
2020-06-16 12:34:43 +0200

    Close RandomAccessFile object

--- 201-large-static-content/src/test/java/io/quarkus/qe/content/GenerateLargeFile.java
+++ 201-large-static-content/src/test/java/io/quarkus/qe/content/GenerateLargeFile.java
@@ -5,9 +5,10 @@ import java.io.RandomAccessFile;
 public class GenerateLargeFile {
     public static void main(String args[]) throws Exception {
 
-        // args[0] is expected to be something like /Users/rsvoboda/git/beefy-scenarios/scenario-201-static/target/classes
+        // args[0] is expected to be something like /Users/rsvoboda/git/beefy-scenarios/201-large-static-content/target/classes
         RandomAccessFile f = new RandomAccessFile(args[0] + "/META-INF/resources/big-file", "rw");
         // avoid https://github.com/quarkusio/quarkus/issues/2917
         f.setLength(1024 * 1024 * 1023);
+        f.close();
     }
 }
