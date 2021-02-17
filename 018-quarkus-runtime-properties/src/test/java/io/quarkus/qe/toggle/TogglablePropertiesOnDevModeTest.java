140f3fea3984ecf56937e5ddc48eedb2a233f35f
Jose <josecarvajalhilario@gmail.com>
2021-02-17 11:56:45 +0100

    Add scenario for runtime properties

---
+++ 018-quarkus-runtime-properties/src/test/java/io/quarkus/qe/toggle/TogglablePropertiesOnDevModeTest.java
@@ -0,0 +1,23 @@
+package io.quarkus.qe.toggle;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.test.QuarkusDevModeTest;
+
+public class TogglablePropertiesOnDevModeTest extends BaseTogglablePropertiesTest {
+
+    private static final String APPLICATION_PROPERTIES = "application.properties";
+
+    @RegisterExtension
+    static final QuarkusDevModeTest app = new QuarkusDevModeTest()
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addAsResource(APPLICATION_PROPERTIES, APPLICATION_PROPERTIES));
+
+    @Override
+    protected void whenChangeServiceAtRuntime(TogglableServices service, boolean enable) {
+        app.modifyResourceFile(APPLICATION_PROPERTIES,
+                s -> s.replace(service.getToggleProperty() + "=" + !enable, service.getToggleProperty() + "=" + enable));
+    }
+}
