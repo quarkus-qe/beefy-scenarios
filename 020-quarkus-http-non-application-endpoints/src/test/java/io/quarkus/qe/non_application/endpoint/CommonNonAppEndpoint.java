54c4c0d10997a8d58195337c376822af643b0d7f
pablo gonzalez granados <pagonzal@redhat.com>
2021-03-01 12:44:55 +0100

    Non-appliaction endpoints coverage

---
+++ 020-quarkus-http-non-application-endpoints/src/test/java/io/quarkus/qe/non_application/endpoint/CommonNonAppEndpoint.java
@@ -0,0 +1,37 @@
+package io.quarkus.qe.non_application.endpoint;
+
+import static io.restassured.RestAssured.given;
+
+import java.util.Arrays;
+import java.util.List;
+
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.specification.RequestSpecification;
+
+public abstract class CommonNonAppEndpoint {
+    protected static final String ROOT_BASE_PATH = "/api/";
+    private RequestSpecification request;
+    private RequestSpecBuilder spec;
+
+    protected final List<String> nonAppEndpoints = Arrays.asList(
+            "/openapi", "/metrics/base", "/metrics/application",
+            "/metrics/vendor", "/metrics", "/health/group", "/health/well", "/health/ready",
+            "/health/live", "/health");
+
+    protected void givenBasePath(String basePath) {
+        RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
+        this.spec = new RequestSpecBuilder();
+        this.spec.setBasePath(basePath);
+    }
+
+    protected void whenMakeRequestOverNonAppEndpoints() {
+        this.request = spec.build();
+    }
+
+    protected void thenStatusCodeShouldBe(int status) {
+        for (String endpoint : nonAppEndpoints) {
+            given().spec(request).log().uri().expect().statusCode(status).when().get(endpoint);
+        }
+    }
+}
---
+++ 020-quarkus-http-non-application-endpoints/src/test/java/io/quarkus/qe/non_application/endpoint/RelativePathNonAppEndpointTest.java
@@ -0,0 +1,37 @@
+package io.quarkus.qe.non_application.endpoint;
+
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.RegisterExtension;
+
+import io.quarkus.qe.http.non_application.endpoint.HelloResource;
+import io.quarkus.test.QuarkusProdModeTest;
+
+public class RelativePathNonAppEndpointTest extends CommonNonAppEndpoint {
+    private static final String BASE_PATH = "q";
+
+    @RegisterExtension
+    static final QuarkusProdModeTest relativePathScenario = new QuarkusProdModeTest()
+            .overrideConfigKey("quarkus.http.root-path", "/api")
+            .overrideConfigKey("quarkus.http.non-application-root-path", BASE_PATH)
+            .overrideConfigKey("quarkus.http.redirect-to-non-application-root-path", "false")
+            .setArchiveProducer(() -> ShrinkWrap.create(JavaArchive.class)
+                    .addClass(HelloResource.class));
+
+    @BeforeEach
+    public void beforeEach() {
+        relativePathScenario.stop();
+        relativePathScenario.start();
+    }
+
+    @Test
+    @DisplayName("Non-application relative path")
+    public void nonAppEndpointsWithRootPathAndNonAppRootPath() {
+        givenBasePath(ROOT_BASE_PATH + BASE_PATH);
+        whenMakeRequestOverNonAppEndpoints();
+        thenStatusCodeShouldBe(200);
+    }
+}
