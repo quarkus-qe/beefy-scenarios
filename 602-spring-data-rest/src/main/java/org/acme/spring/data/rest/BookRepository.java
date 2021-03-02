4bfb888a8acae2ead1106c0a23e074bdada76c1e
Jose <josecarvajalhilario@gmail.com>
2021-03-02 15:20:38 +0100

    Adjustments to spring-data-rest module

--- 602-spring-data-rest/src/main/java/org/acme/spring/data/rest/BookRepository.java
+++ 602-spring-data-rest/src/main/java/org/acme/spring/data/rest/BookRepository.java
@@ -1,27 +1,29 @@
 package org.acme.spring.data.rest;
 
-import com.sun.xml.bind.v2.model.core.ID;
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
-import java.util.List;
-import java.util.Optional;
-
 @RepositoryRestResource(exported=false, path = "books", collectionResourceRel = "books")
 public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
 
+    @Override
     @RestResource(exported = true)
     Page<Book> findAll(Pageable pageable);
 
     @RestResource(exported = true)
     List<Book> findByOrderByNameDesc();
 
+    @Override
     @RestResource(path = "id")
-    Optional<Book> findById(ID id);
+    Optional<Book> findById(Long id);
 
+    @Override
     @RestResource()
     Book save(Book book);
 }
