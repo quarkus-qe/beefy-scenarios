d7cbfb950bfb1aa675c9e6400e4f1b46d7691a9d
Pablo Gonzalez Granados <pagonzal@redhat.com>
2021-03-23 17:30:06 +0100

    Avoid mutiny/blocking grpc be under the same service in order to avoid service.name collisions. (#155)

--- 009-quarkus-infinispan-grpc-kafka/src/main/java/io/quarkus/qe/greeting/BlockingHelloService.java
+++ 009-quarkus-infinispan-grpc-kafka/src/main/java/io/quarkus/qe/greeting/BlockingHelloService.java
@@ -1,14 +1,15 @@
 package io.quarkus.qe.greeting;
 
+import io.grpc.stub.StreamObserver;
+import io.quarkus.example.blocking.GreeterGrpc;
+import io.quarkus.example.dto.HelloReply;
+import io.quarkus.example.dto.HelloRequest;
 import javax.inject.Singleton;
 
-import io.grpc.stub.StreamObserver;
-import io.quarkus.example.GreeterGrpc;
-import io.quarkus.example.HelloReply;
-import io.quarkus.example.HelloRequest;
+
 
 @Singleton
-public class HelloService extends GreeterGrpc.GreeterImplBase {
+public class BlockingHelloService extends GreeterGrpc.GreeterImplBase{
 
     @Override
     public void sayHello(HelloRequest request, StreamObserver<HelloReply> responseObserver) {
