162e9c2f9350351d9b5741b872a6c08aa7c507a1
Josef Smrcka <jsmrcka@redhat.com>
2021-04-08 09:20:20 +0200

    Add new spring-web scenario
    Based on
    https://github.com/eugenp/tutorials/tree/master/spring-boot-modules/spring-boot-bootstrap

--- 603-spring-web-smallrye-openapi/README.md
+++ 603-spring-web-smallrye-openapi/README.md
@@ -1,3 +1,7 @@
+# Table of contents
+1. [Quarkus Spring Web + Quarkus SmallRye OpenAPI](#quarkus-spring-web--quarkus-smallrye-openapi)
+1. [Quarkus Spring Web - Spring Boot Bootstrap application](#quarkus-spring-web---spring-boot-bootstrap-application)
+
 # Quarkus Spring Web + Quarkus SmallRye OpenAPI
 
 ## Issues references
@@ -12,6 +16,25 @@ Verify correct content types in OpenAPI endpoint output (`/q/openapi`).
 - Generate OpenAPI schema using `quarkus-smallrye-openapi`.
 - Access the OpenAPI schema by invoking `/q/openapi` endpoint.
 - Verify content types in the schema. The types should correspond to the definitions in Spring annotations.
+
+## Requirements
+- JDK 1.8+
+- GraalVM
+
+# Quarkus Spring Web - Spring Boot Bootstrap application
+
+## Issues references
+[Document: Using the Quarkus Extension for Spring Web API](https://issues.redhat.com/browse/QUARKUS-185)
+
+[Spring Web: @ExceptionHandler response does not override @ResponseStatus](https://github.com/quarkusio/quarkus/issues/16321)
+
+## Scope of the testing
+Test CRUD resource `BookController`:
+- Verify response status.
+- Verify modified data in write operations.
+- Verify custom error handler.
+
+Test home page HTML response and value injection in `SimpleController`.
 
 ## Requirements
 - JDK 1.8+
---
+++ 603-spring-web-smallrye-openapi/src/main/java/io/quarkus/qe/books/web/BookController.java
@@ -0,0 +1,74 @@
+package io.quarkus.qe.books.web;
+
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import io.quarkus.qe.books.persistence.model.Book;
+import io.quarkus.qe.books.persistence.repo.BookRepository;
+import io.quarkus.qe.books.web.exception.BookIdMismatchException;
+import io.quarkus.qe.books.web.exception.BookNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/books")
+public class BookController {
+
+    @Autowired
+    BookRepository bookRepository;
+
+    @GetMapping
+    public Iterable<Book> findAll() {
+        return bookRepository.listAll();
+    }
+
+    @GetMapping("/title/{bookTitle}")
+    public List<Book> findByTitle(@PathVariable String bookTitle) {
+        return bookRepository.findByTitle(bookTitle);
+    }
+
+    @GetMapping("/{id}")
+    public Book findOne(@PathVariable long id) {
+        return bookRepository.findByIdOptional(id)
+          .orElseThrow(BookNotFoundException::new);
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    @Transactional
+    public Book create(@RequestBody Book book) {
+        bookRepository.persist(book);
+        return book;
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void delete(@PathVariable long id) {
+        bookRepository.findByIdOptional(id)
+          .orElseThrow(BookNotFoundException::new);
+        bookRepository.deleteById(id);
+    }
+
+    @PutMapping("/{id}")
+    @Transactional
+    public Book updateBook(@RequestBody Book book, @PathVariable long id) {
+        if (book.getId() != id) {
+            throw new BookIdMismatchException();
+        }
+        final Book savedBook = bookRepository.findByIdOptional(id)
+                .orElseThrow(BookNotFoundException::new);
+        savedBook.setAuthor(book.getAuthor());
+        savedBook.setTitle(book.getTitle());
+        return book;
+    }
+}
