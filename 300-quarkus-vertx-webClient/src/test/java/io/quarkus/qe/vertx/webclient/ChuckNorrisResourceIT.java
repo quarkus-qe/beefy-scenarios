5396004043645fe1d26839c66f0baa7ed96ba8f0
pablo gonzalez granados <pagonzal@redhat.com>
2021-04-30 10:43:50 +0200

    Add debug native symbols generation test

--- 300-quarkus-vertx-webClient/src/test/java/io/quarkus/qe/vertx/webclient/ChuckNorrisResourceIT.java
+++ 300-quarkus-vertx-webClient/src/test/java/io/quarkus/qe/vertx/webclient/ChuckNorrisResourceIT.java
@@ -1,7 +1,23 @@
 package io.quarkus.qe.vertx.webclient;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.nio.file.Paths;
+
+import org.junit.jupiter.api.Test;
+
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
 public class ChuckNorrisResourceIT extends ChuckNorrisResourceTest {
+
+    private static final String DEBUG_SYMBOLS_FILE_NAME = "300-quarkus-vertx-webclient-1.0.0-SNAPSHOT-runner.debug";
+
+    @Test
+    public void checkNativeDebugSymbols() throws MalformedURLException {
+        File debugFile = Paths.get("target", DEBUG_SYMBOLS_FILE_NAME).toFile();
+        assertThat("Missing debug symbols file: " + DEBUG_SYMBOLS_FILE_NAME, debugFile.exists());
+    }
 }
