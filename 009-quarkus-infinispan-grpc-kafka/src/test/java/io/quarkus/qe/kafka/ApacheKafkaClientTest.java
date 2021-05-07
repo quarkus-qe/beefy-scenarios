417cadaa0341b70cae668d88a6382d6cb20275de
Pablo Gonzalez Granados <pagonzal@redhat.com>
2021-05-07 15:52:39 +0200

    Add Quarkus Apache kafka client coverage (#200)

---
+++ 009-quarkus-infinispan-grpc-kafka/src/test/java/io/quarkus/qe/kafka/ApacheKafkaClientTest.java
@@ -0,0 +1,39 @@
+package io.quarkus.qe.kafka;
+
+import static io.restassured.RestAssured.get;
+import static io.restassured.RestAssured.given;
+import static org.awaitility.Awaitility.await;
+import static org.hamcrest.core.StringContains.containsString;
+
+import org.junit.jupiter.api.Test;
+
+import io.quarkus.qe.containers.StrimziKafkaResource;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.junit.QuarkusTest;
+
+@QuarkusTest
+@QuarkusTestResource(StrimziKafkaResource.class)
+public class ApacheKafkaClientTest {
+    @Test
+    void testBareClients() {
+        await().untilAsserted(() -> {
+            given()
+                    .queryParam("key", "my-key")
+                    .queryParam("value", "my-value")
+                    .when()
+                    .post("/kafka")
+                    .then()
+                    .statusCode(200);
+
+            get("/kafka")
+                    .then()
+                    .statusCode(200)
+                    .body(containsString("my-key-my-value"));
+        });
+
+        get("/kafka/topics")
+                .then()
+                .statusCode(200)
+                .body(containsString("hello"));
+    }
+}
