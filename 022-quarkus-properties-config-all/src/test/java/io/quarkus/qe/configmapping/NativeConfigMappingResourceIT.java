8af91106d77fe6ac308274f8a25f04e988e7b7b5
Jose <josecarvajalhilario@gmail.com>
2021-06-08 15:36:12 +0200

    Enable tests for quarkus/issues/17653
    The issue https://github.com/quarkusio/quarkus/issues/17653 has been fixed, so we can enable the tests back.
    I confirmed that these tests are now working fine.

--- 022-quarkus-properties-config-all/src/test/java/io/quarkus/qe/configmapping/NativeConfigMappingResourceIT.java
+++ 022-quarkus-properties-config-all/src/test/java/io/quarkus/qe/configmapping/NativeConfigMappingResourceIT.java
@@ -1,10 +1,7 @@
 package io.quarkus.qe.configmapping;
 
-import org.junit.jupiter.api.Disabled;
-
 import io.quarkus.test.junit.NativeImageTest;
 
-@Disabled("TODO: Can't add configsource.properties because of https://github.com/quarkusio/quarkus/issues/17653")
 @NativeImageTest
 public class NativeConfigMappingResourceIT extends ConfigMappingResourceTest {
 }
