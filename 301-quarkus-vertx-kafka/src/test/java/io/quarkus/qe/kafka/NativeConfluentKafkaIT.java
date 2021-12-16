58fe6a7e0a2dd2ff4439710c3abc640e719a9b4b
Josef Smrcka <jsmrcka@redhat.com>
2021-12-16 13:05:28 +0100

    Disable native build in `300-quarkus-vertx-webClient` and `301-quarkus-vertx-kafka` modules
    Workaround to https://github.com/quarkusio/quarkus/issues/22275.

--- 301-quarkus-vertx-kafka/src/test/java/io/quarkus/qe/kafka/NativeConfluentKafkaIT.java
+++ 301-quarkus-vertx-kafka/src/test/java/io/quarkus/qe/kafka/NativeConfluentKafkaIT.java
@@ -1,7 +1,10 @@
 package io.quarkus.qe.kafka;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.junit.NativeImageTest;
 
+@Disabled("https://github.com/quarkusio/quarkus/issues/22275")
 @NativeImageTest
 public class NativeConfluentKafkaIT extends ConfluentKafkaTest {
 
