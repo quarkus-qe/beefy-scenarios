dabebde178288c6ffbc518a58d427c5b8e63ab23
Rostislav Svoboda <rsvoboda@redhat.com>
2022-04-25 18:33:00 +0200

    Disable native because of broken opentelemetry-exporter-jaeger

--- 301-quarkus-vertx-kafka/src/test/java/io/quarkus/qe/kafka/NativeStrimziKafkaIT.java
+++ 301-quarkus-vertx-kafka/src/test/java/io/quarkus/qe/kafka/NativeStrimziKafkaIT.java
@@ -1,8 +1,11 @@
 package io.quarkus.qe.kafka;
 
+import org.junit.jupiter.api.Disabled;
+
 import io.quarkus.test.junit.NativeImageTest;
 
 @NativeImageTest
+@Disabled("https://github.com/quarkusio/quarkus/issues/25143")
 public class NativeStrimziKafkaIT extends StrimziKafkaTest {
 
     @Override
