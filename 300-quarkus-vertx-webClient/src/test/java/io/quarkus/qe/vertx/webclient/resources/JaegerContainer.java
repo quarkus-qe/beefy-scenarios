ec00809131b2a5b672a786c1cf4308e5c5223274
pablo gonzalez granados <pagonzal@redhat.com>
2022-10-05 11:57:16 +0200

    Drop OpenTelemetry Jaeger exporter

--- 300-quarkus-vertx-webClient/src/test/java/io/quarkus/qe/vertx/webclient/resources/JaegerContainer.java
+++ 300-quarkus-vertx-webClient/src/test/java/io/quarkus/qe/vertx/webclient/resources/JaegerContainer.java
@@ -4,13 +4,15 @@ import org.testcontainers.containers.GenericContainer;
 import org.testcontainers.containers.wait.strategy.LogMessageWaitStrategy;
 
 public class JaegerContainer extends GenericContainer<JaegerContainer> {
-    public static final int REST_PORT = 16686;
-    private static final int TRACE_PORT = 14250;
+    private static final String COLLECTOR_OTLP_ENABLED = "COLLECTOR_OTLP_ENABLED";
+    private static final int OLTP_PORT = 4317;
+    private static final int TRACE_PORT = 16686;
 
     public JaegerContainer() {
-        super("jaegertracing/all-in-one:latest");
+        super("quay.io/jaegertracing/all-in-one:1.37.0");
         waitingFor(new LogMessageWaitStrategy().withRegEx(".*\"Health Check state change\",\"status\":\"ready\".*\\s"));
-        addFixedExposedPort(REST_PORT, REST_PORT);
+        addFixedExposedPort(OLTP_PORT, OLTP_PORT);
         addFixedExposedPort(TRACE_PORT, TRACE_PORT);
+        addEnv(COLLECTOR_OTLP_ENABLED, "true");
     }
 }
