283d105b787ae3d11813b7a4fc6fa3746196e0da
Michal Vavřík <mvavrik@redhat.com>
2023-03-21 11:14:54 +0100

    Fix MultiplePersistenceUnitTest as id pool by default has gaps of 50

--- 021-quarkus-panache-multiple-pus/src/main/resources/import-vegetables.sql
+++ 021-quarkus-panache-multiple-pus/src/main/resources/import-vegetables.sql
@@ -6,4 +6,4 @@ INSERT INTO vegetable (id, name) VALUES (nextval('Vegetable_SEQ'), 'Kohlrabi');
 INSERT INTO vegetable (id, name) VALUES (nextval('Vegetable_SEQ'), 'Leek');
 INSERT INTO vegetable (id, name) VALUES (nextval('Vegetable_SEQ'), 'Onion');
 INSERT INTO vegetable (id, name) VALUES (nextval('Vegetable_SEQ'), 'Garlic');
-ALTER SEQUENCE Vegetable_SEQ RESTART with 57; -- 7 + pool size TODO: https://github.com/quarkusio/quarkus/issues/31481
+ALTER SEQUENCE Vegetable_SEQ RESTART with 8;
