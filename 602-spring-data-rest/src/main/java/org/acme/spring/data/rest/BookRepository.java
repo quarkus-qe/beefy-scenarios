c926c54557d3289dda567d242bc8eb679fda8ddc
Jakub Jedlicka <jjedlick@redhat.com>
2024-06-12 14:23:56 +0200

    Add `CrudRepository` as Quarkus move spring data to version 3.2

--- 602-spring-data-rest/src/main/java/org/acme/spring/data/rest/BookRepository.java
+++ 602-spring-data-rest/src/main/java/org/acme/spring/data/rest/BookRepository.java
@@ -5,12 +5,13 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.data.rest.core.annotation.RestResource;
 
 @RepositoryRestResource(exported = false, path = "books", collectionResourceRel = "books")
-public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
+public interface BookRepository extends PagingAndSortingRepository<Book, Long>, CrudRepository<Book, Long> {
 
     @Override
     @RestResource(exported = true)
