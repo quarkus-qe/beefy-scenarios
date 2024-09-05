e544340922f7115468ddc2a6cde5afb9bc6a4f75
Jakub Jedlicka <jjedlick@redhat.com>
2024-09-05 09:56:03 +0200

    Change parameters for WithTestResource annotation

--- 004-quarkus-HHH-and-HV/src/test/java/io/quarkus/qe/hibernate/validator/TestResources.java
+++ 004-quarkus-HHH-and-HV/src/test/java/io/quarkus/qe/hibernate/validator/TestResources.java
@@ -1,8 +1,9 @@
 package io.quarkus.qe.hibernate.validator;
 
+import io.quarkus.test.common.TestResourceScope;
 import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.h2.H2DatabaseTestResource;
 
-@WithTestResource(value = H2DatabaseTestResource.class, restrictToAnnotatedClass = false)
+@WithTestResource(value = H2DatabaseTestResource.class, scope = TestResourceScope.MATCHING_RESOURCES)
 public class TestResources {
 }
