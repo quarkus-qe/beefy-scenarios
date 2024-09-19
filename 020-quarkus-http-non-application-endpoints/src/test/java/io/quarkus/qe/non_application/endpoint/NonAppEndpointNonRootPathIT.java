bed24b5d156a1bc2c858f0c0a58dcd556db802da
Georgii Troitskii <gtroitsk@redhat.com>
2024-09-19 14:26:07 +0200

    Enable tests in native as build time is acceptable now

--- 020-quarkus-http-non-application-endpoints/src/test/java/io/quarkus/qe/non_application/endpoint/NonAppEndpointNonRootPathIT.java
+++ 020-quarkus-http-non-application-endpoints/src/test/java/io/quarkus/qe/non_application/endpoint/NonAppEndpointNonRootPathIT.java
@@ -3,10 +3,8 @@ package io.quarkus.qe.non_application.endpoint;
 import static io.quarkus.qe.non_application.endpoint.CommonNonAppEndpoint.NATIVE;
 import static io.quarkus.qe.non_application.endpoint.CommonNonAppEndpoint.QUARKUS_PROFILE;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.condition.EnabledIfSystemProperty;
 
-@Disabled(value = "Due to high execution time to build app on Native")
 @EnabledIfSystemProperty(named = QUARKUS_PROFILE, matches = NATIVE)
 public class NonAppEndpointNonRootPathIT extends NonAppEndpointNonRootPathTest {
 }
