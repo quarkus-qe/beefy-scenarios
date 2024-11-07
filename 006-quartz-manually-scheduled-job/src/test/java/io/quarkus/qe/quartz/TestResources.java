cca16157e146fe20d68c5c5d8c7b093cd8b959b6
Jakub Jedlicka <jjedlick@redhat.com>
2024-11-07 01:32:28 +0100

    Fix and refactor the scope of some resources

--- 006-quartz-manually-scheduled-job/src/test/java/io/quarkus/qe/quartz/TestResources.java
+++ 006-quartz-manually-scheduled-job/src/test/java/io/quarkus/qe/quartz/TestResources.java
@@ -4,6 +4,6 @@ import io.quarkus.test.common.TestResourceScope;
 import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.h2.H2DatabaseTestResource;
 
-@WithTestResource(value = H2DatabaseTestResource.class, scope = TestResourceScope.MATCHING_RESOURCES)
+@WithTestResource(value = H2DatabaseTestResource.class, scope = TestResourceScope.GLOBAL)
 public class TestResources {
 }
