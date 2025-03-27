b4c1437f6b214ba4d9ed4c300c68b7e02e0407a5
Rostislav Svoboda <rosta.svo@gmail.com>
2025-03-27 14:46:09 +0100

    Docker/Podman is not needed for 003-quarkus-many-extensions

--- 003-quarkus-many-extensions/src/test/java/io/quarkus/qe/many/extensions/ManyExtensionsResourceTest.java
+++ 003-quarkus-many-extensions/src/test/java/io/quarkus/qe/many/extensions/ManyExtensionsResourceTest.java
@@ -5,12 +5,9 @@ import static org.hamcrest.CoreMatchers.is;
 
 import org.junit.jupiter.api.Test;
 
-import io.quarkus.qe.many.extensions.containers.KeycloakTestResource;
-import io.quarkus.test.common.WithTestResource;
 import io.quarkus.test.junit.QuarkusTest;
 
 @QuarkusTest
-@WithTestResource(value = KeycloakTestResource.class)
 public class ManyExtensionsResourceTest {
 
     @Test
